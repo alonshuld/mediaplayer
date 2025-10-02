8358955998690d81ef0754c9001bee2dfa15d95e
alonshuld <alonshuld@gmail.com>
2025-10-02 17:41:52 +0300

    started using all 4 songs

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,8 +3,14 @@ import { Layout } from "antd";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { SongList } from "./SongList";
-import songCover from "../assets/songs/1.png";
-import songFile from "../assets/songs/1.mp3";
+import songCover1 from "../assets/songs/1.png";
+import songFile1 from "../assets/songs/1.mp3";
+import songCover2 from "../assets/songs/2.png";
+import songFile2 from "../assets/songs/2.mp3";
+import songCover3 from "../assets/songs/3.png";
+import songFile3 from "../assets/songs/3.mp3";
+import songCover4 from "../assets/songs/4.png";
+import songFile4 from "../assets/songs/4.mp3";
 import "./HomePage.css";
 
 export const HomePage: FC = () => {
@@ -46,22 +52,29 @@ export const HomePage: FC = () => {
                 id: "1",
                 title: "הספר הכחול",
                 artist: "פאר טסי",
-                songPath: songFile,
-                coverPath: songCover,
+                songPath: songFile1,
+                coverPath: songCover1,
               },
               {
                 id: "2",
                 title: "תסמינים של פרידה",
                 artist: "עומר אדם",
-                songPath: songFile,
-                coverPath: songCover,
+                songPath: songFile2,
+                coverPath: songCover2,
               },
               {
                 id: "3",
                 title: "עד אחרי הנצח",
                 artist: "פאר טסי",
-                songPath: songFile,
-                coverPath: songCover,
+                songPath: songFile3,
+                coverPath: songCover3,
+              },
+              {
+                id: "4",
+                title: "מה יהיה מחר",
+                artist: "פאר טסי",
+                songPath: songFile4,
+                coverPath: songCover4,
               },
             ]}
           />
