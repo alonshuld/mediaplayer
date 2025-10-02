f95b738d9ca62f6eaeb521b50d643333d5dbaf1d
alonshuld <alonshuld@gmail.com>
2025-10-02 16:29:57 +0300

    added credit

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,18 @@
-import { type FC } from 'react';
-import { Typography } from 'antd';
-import { Disc3 } from 'lucide-react';
-import './Logo.css';
+import { type FC } from "react";
+import { Typography } from "antd";
+import { Disc3 } from "lucide-react";
+import "./Logo.css";
 
 const { Title } = Typography;
 
 export const Logo: FC = () => {
-    return (
-        <div className="Logo">
-            <Disc3 size={44}/>
-            <Title level={1}>din</Title>
-        </div>
-    );
+  return (
+    <div className="Logo">
+      <Disc3 size={44} />
+      <Title level={1}>din</Title>
+      <div className="Credit">
+        <Title level={3}>powered by AlonSD</Title>
+      </div>
+    </div>
+  );
 };
